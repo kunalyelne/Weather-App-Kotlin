7b78954def4e004e539a20de4265393530385639
Kunal Yelne <kunal17058@gmail.com>
2021-05-11 17:49:17 +0530

    Add Portrait Screen Orientation for all activity

--- app/src/main/java/com/kyodude/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/kyodude/weatherapp/WeatherApplication.kt
@@ -1,7 +1,38 @@
 package com.kyodude.weatherapp
 
+import android.app.Activity
 import android.app.Application
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class WeatherApplication: Application()
+class WeatherApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks {
+            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+                p0.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+            }
+
+            override fun onActivityStarted(p0: Activity) {
+            }
+
+            override fun onActivityResumed(p0: Activity) {
+            }
+
+            override fun onActivityPaused(p0: Activity) {
+            }
+
+            override fun onActivityStopped(p0: Activity) {
+            }
+
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+            }
+
+            override fun onActivityDestroyed(p0: Activity) {
+            }
+
+        })
+    }
+}
